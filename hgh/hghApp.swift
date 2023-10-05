27bf1acfd76ee7d31a8134222b5c64b42ffd182f
asmamohds <asmamohds.v@gmail.com>
2023-10-05 11:57:57 +0300

    Initial Commit

---
+++ hgh/hghApp.swift
@@ -0,0 +1,17 @@
+//
+//  hghApp.swift
+//  hgh
+//
+//  Created by asma on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct hghApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
